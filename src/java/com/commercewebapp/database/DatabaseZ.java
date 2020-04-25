d4994ff4c15880707f72123d95e127a4a7089287
Fidel Sorto <fidel.sorto@hotmail.com>
2020-04-25 11:01:43 -0600

    pull

--- src/java/com/commercewebapp/database/DatabaseZ.java
+++ src/java/com/commercewebapp/database/DatabaseZ.java
@@ -55,7 +55,7 @@ public class DatabaseZ {
             try {
                 Class.forName("com.mysql.cj.jdbc.Driver");
                 localConnection= DriverManager.getConnection("jdbc:mysql://localhost/comercebd?"
-                    + "user=root&password=12345678B"
+                    + "user=root&password=root"
                     + "&allowPublicKeyRetrieval=true"
                     + "&usUnicode=true&useJDBCCompliantTimeZoneShift=true"
                     + "&useLegacyDateTimeCode=false&serverTimezone=UTC"
