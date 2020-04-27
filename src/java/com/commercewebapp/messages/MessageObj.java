4a9836afb51d0f95aad87c2d4cc03b4418ffc3c9
Jakelin Rivera <60167444+Jakelin-prog@users.noreply.github.com>
2020-04-26 20:55:54 -0600

    MessageObj Modificado

--- src/java/com/commercewebapp/messages/MessageObj.java
+++ src/java/com/commercewebapp/messages/MessageObj.java
@@ -11,12 +11,12 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class MessageObj {
-    public boolean enviarCorreo(String mensaje, String asunto) throws AddressException, MessagingException{
+    public boolean enviarCorreo(String p_nombre, String p_destinatario, String p_montoTotal ) throws AddressException, MessagingException{
     boolean enviado = false;
         
             String de = "pruebapolux@gmail.com"; 
             String clave = "mbbvr9788";
-            String para="jaky47@gmail.com";  
+            String para="p_destinatario";  
             String host = "smtp.gmail.com";
 
             Properties prop = System.getProperties();
@@ -36,8 +36,12 @@ public class MessageObj {
             
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(para));
 
-            message.setSubject(asunto);
-            message.setText(mensaje);
+            message.setSubject("BrocOnline");
+            message.setContent("<div style='border: green 10px inset;'><table>"
+                    + "<tr><th></td><td><div><h2 style=\"font-family: serif;\">"  +p_nombre+", su compra se ha realizado exitosamente!</h4></div></td></tr></table>"
+                    +"<table><tr><th><h4>Usted ha cancelado un total de $"+p_montoTotal+"</h4></tr>"
+                    +"<tr><img src=\"images/Footer.png\"></tr></table></div>"
+                , "text/html; charset=utf-8");
 
             Transport transport = sesion.getTransport("smtp");
 
